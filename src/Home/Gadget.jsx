9932e8a28ae283eabdb7c52979566880b86b1ad9
firoz.nur <firoznur5@gmail.com>
2024-08-16 23:05:06 +0600

    Add : Gadget Card

---
+++ src/Home/Gadget.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+
+const ProductCard = ({ product }) => {
+  console.log(product);
+  return (
+    <div className="group flex h-full flex-col justify-between overflow-hidden rounded bg-white text-slate-500 shadow-xl shadow-blue-200 hover:scale-105 hover:border hover:border-blue-400 hover:shadow-none">
+      <>
+        <figure>
+          <img
+            src={product?.image}
+            alt="card image"
+            className="aspect-video w-full object-contain p-4"
+          />
+        </figure>
+
+        <div className="p-6">
+          <header className="mb-4">
+            <h3 className="text-xl font-medium text-slate-700">
+              {product?.name}
+            </h3>
+            <p className="text-slate-400">${product?.price}</p>
+            <p className="text-slate-500">Category : {product?.category}</p>
+            <p className="text-slate-500">Ratings : {product?.ratings}</p>
+            <p className="text-slate-500">Launch Date : {product?.time}</p>
+          </header>
+          <p>{product?.description}</p>
+        </div>
+      </>
+
+      <div className="flex justify-end p-6 pt-0">
+        <button className="inline-flex h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded bg-blue-500 px-5 text-sm font-medium tracking-wide text-white transition duration-300 hover:bg-blue-600 focus:bg-blue-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-blue-300 disabled:bg-blue-300 disabled:shadow-none">
+          <span>Order now!</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+ProductCard.propTypes = {
+  product: PropTypes.object,
+};
+
+export default ProductCard;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,21 +1,38 @@
 import React, { useEffect, useState } from "react";
+import Gadget from "./Gadget";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const [gadgets, setGadgets] = useState([]);
   const [search, setSearch] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
 
-  console.log(products);
+  console.log(gadgets);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_SERVER}/products?page=${currentPage - 1}`)
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data?.result);
+        setGadgets(data?.result);
       });
   }, [search]);
 
-  return <div>ami {products.length}</div>;
+  return (
+    <div className="mx-auto container">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {gadgets.length > 0 ? (
+          gadgets.map((gadget) => (
+            <Gadget key={gadget._id} product={gadget}></Gadget>
+          ))
+        ) : (
+          <div className="col-span-1 md:col-span-2 lg:col-span-4">
+            <h3 className="flex h-[50vh] items-center justify-center text-center text-3xl font-semibold">
+              Nothing Found!
+            </h3>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
