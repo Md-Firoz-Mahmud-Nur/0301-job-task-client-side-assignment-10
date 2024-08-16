dce309dee679aab36cd9302040608aa2542a7c47
firoz.nur <firoznur5@gmail.com>
2024-08-16 20:43:58 +0600

    Add : Get Products Data

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <div>
-      ami
-      
-    </div>
-  );
+  const [products, setProducts] = useState([]);
+  console.log(products);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_SERVER}/products`)
+    .then((res) =>res.json()
+    .then((data) => setProducts(data)),
+    );
+  }),[];
+
+  return <div>ami {products.length}</div>;
 };
 
 export default Home;
