1a84c46d214bc39a85f49426d11aa3c9e4794516
firoz.nur <firoznur5@gmail.com>
2024-08-16 22:20:47 +0600

    Get Data To Home Page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
   console.log(products);
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_SERVER}/products`)
-    .then((res) =>res.json()
-    .then((data) => setProducts(data)),
-    );
-  }),[];
+    fetch(`${import.meta.env.VITE_SERVER}/products?page=${currentPage - 1}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data?.result);
+      });
+  }, [search]);
 
   return <div>ami {products.length}</div>;
 };
