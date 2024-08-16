5e826d08ffa8e2ef12d0af7284c056fe247d0d95
firoz.nur <firoznur5@gmail.com>
2024-08-16 10:34:18 +0600

    Add : Error Page and Auth Provider

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      ami
+      
+    </div>
+  );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Home/Home";
+import Root from "./Root";
+import ErrorPage from "./ErrorPage";
+import AuthProvider from "./AuthProvider";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
-)
+);
