4e69d4d6342cd58e2c94e24a89b8bf105e24efbb
firoz.nur <firoznur5@gmail.com>
2024-08-18 15:50:15 +0600

    Fix : Pagination Won't Show When No Data Available

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -2,19 +2,21 @@ const Pagination = ({ currentPage, pages, setCurrentPage }) => {
   return (
     <div className="mt-8 flex justify-center">
       <ul className="flex space-x-1 md:space-x-2">
-        <li>
-          <button
-            onClick={() => setCurrentPage(currentPage - 1)}
-            className={`${
-              currentPage === 1
-                ? "cursor-not-allowed bg-gray-300 text-gray-700"
-                : "bg-gray-300 text-gray-700 hover:bg-opacity-80"
-            } rounded-l-lg px-2 py-1 focus:outline-none md:px-4 md:py-2`}
-            disabled={currentPage === 1}
-          >
-            Prev
-          </button>
-        </li>
+        {pages.length > 0 && (
+          <li>
+            <button
+              onClick={() => setCurrentPage(currentPage - 1)}
+              className={`${
+                currentPage === 1
+                  ? "cursor-not-allowed bg-gray-300 text-gray-700"
+                  : "bg-gray-300 text-gray-700 hover:bg-opacity-80"
+              } rounded-l-lg px-2 py-1 focus:outline-none md:px-4 md:py-2`}
+              disabled={currentPage === 1}
+            >
+              Prev
+            </button>
+          </li>
+        )}
         {pages.map((page) => (
           <li key={page}>
             <button
@@ -29,19 +31,21 @@ const Pagination = ({ currentPage, pages, setCurrentPage }) => {
             </button>
           </li>
         ))}
-        <li>
-          <button
-            onClick={() => setCurrentPage(currentPage + 1)}
-            className={`${
-              currentPage === pages.length
-                ? "cursor-not-allowed bg-gray-300 text-gray-700"
-                : "bg-gray-300 text-gray-700 hover:bg-opacity-80"
-            } rounded-r-lg px-2 py-1 focus:outline-none md:px-4 md:py-2`}
-            disabled={currentPage === pages.length}
-          >
-            Next
-          </button>
-        </li>
+        {pages.length > 0 && (
+          <li>
+            <button
+              onClick={() => setCurrentPage(currentPage + 1)}
+              className={`${
+                currentPage === pages.length
+                  ? "cursor-not-allowed bg-gray-300 text-gray-700"
+                  : "bg-gray-300 text-gray-700 hover:bg-opacity-80"
+              } rounded-r-lg px-2 py-1 focus:outline-none md:px-4 md:py-2`}
+              disabled={currentPage === pages.length}
+            >
+              Next
+            </button>
+          </li>
+        )}
       </ul>
     </div>
   );
